0adb0d125ce735ffd3ca7ece74d4cf81aa842aeb
milanzagorac <zagoracm@gmail.com>
2021-05-02 12:31:32 -0400

    set up of pet model

--- models.py
+++ models.py
@@ -8,4 +8,11 @@ def connect_db(app):
 
 class Pet(db.Model):
     """ Modeling a pet that is potentially available for adoption """
-      
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, default=True)
+
